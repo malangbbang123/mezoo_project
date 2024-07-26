e126b8d5f2b9eb44a63a8db61839c3e6693c3e7d
bbang_ya <62556038+malangbbang123@users.noreply.github.com>
2024-07-26 16:32:05 +0900

    upload mezoo project files

--- reverse_detection.py
+++ reverse_detection.py
@@ -17,33 +17,24 @@ def rri_ver_2(signal, sr=250):
     return np.array(r_peaks), np.average(rr_interval)
 
 def p_detection(temp):
-    q = np.argmin(temp[40:80]) 
-    p_min = np.argmin(temp[30:q+30])
-    p_max = np.argmax(temp[30:q+30])
-    t_min = np.argmin(temp[120:])
-    t_max = np.argmax(temp[120:])
-    return q, p_min, p_max, t_min, t_max
+    q_peak = np.min(temp[int(len(temp)*0.4)-10:int(len(temp)*0.4)])
+    q_index = np.where(temp == q_peak)[0][0]
+    p_min = np.min(temp[20:q_index-10])
+    p_max = np.max(temp[20:q_index-10])
+    return q_peak, q_index, p_min, p_max
 
-def reverse_caculate(temp, q, p_min, p_max, t_min, t_max):
-    # if temp[p_min+20] > 0:
-    #     title = 0
-    p_min_abs = np.abs(temp[p_min+30])
-    p_max_abs = np.abs(temp[p_max+30])
+def reverse_caculate(p_min, p_max):
+    p_min_abs = np.abs(p_min)
+    p_max_abs = np.abs(p_max)
+    title = 0
     if p_min_abs >= p_max_abs:
-        if (p_min_abs - p_max_abs) <= 0.005:
-            if np.abs(temp[t_min+120]) <= np.abs(temp[t_max+120]):
-                title = 0
-            else:
-                title = 1
-    else:
-        title = 0
-    return title 
+        title = 1
+    return title
 
 def median_filter(signal):
     xx1 = medfilt(signal, 29)
     baseline = medfilt(xx1, 99)
     return signal - baseline
-
 
 def plot_pq(id, reverse, pred_reverse, temp, q, p_min, p_max, t_max, t_min):
     plt.title(f"{id}/{pred_reverse}")
@@ -56,16 +47,7 @@ def plot_pq(id, reverse, pred_reverse, temp, q, p_min, p_max, t_max, t_min):
     plt.legend()
     plt.show()
    
-
-                
-            
-
-
-
-
-        
-
-
-
-
-
+def compute_gradients(x, y):
+    gradients = np.gradient(y, x)
+    return gradients
+                     
